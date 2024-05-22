1b00b0b92c592b1f2a38473bb1f8dc4f79045dfe
David Munoz <davidmunoz@macOS-MGKWNCJDYY.local>
2024-05-22 11:18:45 -0600

    constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
-// export const PROD_API = 'https://sprint-planner-be-production.up.railway.app';
-// export const PROD_WS = 'wss://sprint-planner-be-production.up.railway.app';
+export const PROD_API = 'https://sprint-planner-be-production.up.railway.app';
+export const PROD_WS = 'wss://sprint-planner-be-production.up.railway.app';
 
-export const PROD_API = 'http://localhost:3000';
-export const PROD_WS = 'ws://localhost:3000';
+// export const PROD_API = 'http://localhost:3000';
+// export const PROD_WS = 'ws://localhost:3000';
